e5dc8eca6577e782530c34fec56cd22acb1e734f
Eric Fontaine <eric@ericfontainejazz.com>
2020-08-07 05:54:31 -0400

    ensure proper memory allocation with new JackMetadata(false)

--- common/JackLibGlobals.h
+++ common/JackLibGlobals.h
@@ -29,6 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include "JackTime.h"
 #include "JackClient.h"
 #include "JackError.h"
+#include "JackMetadata.h"
 #include <assert.h>
 #include <signal.h>
 
